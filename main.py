a6c0a229fb360c175f6db363671387adb0330ee8
Filip Kalias <filip040707@gmail.com>
2026-02-19 19:41:02 +0100

    finally?

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from dotenv import load_dotenv
 from discord.ext import commands
 from PIL import Image
 import io
+import asyncio
 import subprocess
 
-bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
 load_dotenv()
 
@@ -17,41 +18,52 @@ load_dotenv()
 async def on_ready():
     print(f"{bot.user} has connected to Discord!")
 
+async def run_git_command(cmd):
+    result = await asyncio.to_thread(
+        subprocess.run,
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+
 @bot.command()
-async def gifthis(ctx):
-    # Check if this message is a reply
-    if ctx.message.reference:
-        # Fetch the original message
-        replied_msg = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+async def gt(ctx):
+    if not ctx.message.reference:
+        await ctx.send("Reply to a message containing an image.")
+        return
 
-        # Check if the replied message has attachments
-        if replied_msg.attachments:
-            attachment = replied_msg.attachments[0]  # take first attachment
-            img_bytes = await attachment.read()  # download the file as bytes
+    replied_msg = await ctx.channel.fetch_message(
+        ctx.message.reference.message_id
+    )
 
-            # Open image with PIL
-            image = Image.open(io.BytesIO(img_bytes))
+    if not replied_msg.attachments:
+        await ctx.send("That message has no image attachment.")
+        return
 
-            # Convert to GIF
-            gif_bytes_io = io.BytesIO()
-            image.save(gif_bytes_io, format="GIF")
-            gif_bytes_io.seek(0)
+    attachment = replied_msg.attachments[0]
+    img_bytes = await attachment.read()
 
-            # Save to storage (example: local folder)
-            randomnum = randrange(1000000, 9999999)
-            filename = f"{randomnum}_{attachment.filename.split('.')[0]}.gif"
-            with open(filename, "wb") as f:
-                f.write(gif_bytes_io.read())
+    image = Image.open(io.BytesIO(img_bytes))
 
-            subprocess.run(["git", "add", filename])
-            subprocess.run(["git", "commit", "-m", f"Add {filename}"])
-            subprocess.run(["git", "push", "origin", "main"])
+    gif_bytes_io = io.BytesIO()
+    image.save(gif_bytes_io, format="GIF")
+    gif_bytes_io.seek(0)
 
-            await ctx.send(f"{filename}")
-        else:
-            await ctx.send("The replied message has no image attachment.")
-    else:
-        await ctx.send("You need to reply to a message with an image.")
+    randomnum = randrange(1000000, 9999999)
+    filename = f"{randomnum}_{attachment.filename.split('.')[0]}.gif"
+
+    with open(filename, "wb") as f:
+        f.write(gif_bytes_io.read())
+
+    await run_git_command(["git", "add", filename])
+    await run_git_command(["git", "commit", "-m", f"Add {filename}"])
+    await run_git_command(["git", "push", "origin", "master"])
+
+    await ctx.send(
+        f"https://github.com/pixousek/kaja.github.io/blob/master/{filename}"
+    )
+
 
 if __name__ == "__main__":
     bot.run(os.getenv("TOKEN"))
